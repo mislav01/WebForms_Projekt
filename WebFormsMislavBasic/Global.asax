3316dbc26a833e7696d61ec9b34468b0f7c09d3a
Mislav Bašić <mislav.basic1@gmail.com>
2019-02-09 02:38:52 +0100

    First commit

---
+++ WebFormsMislavBasic/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="WebFormsMislavBasic.Global" Language="C#" %>
---
+++ WebFormsMislavBasic/Startup.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(WebFormsMislavBasic.Startup))]
+namespace WebFormsMislavBasic
+{
+    public partial class Startup {
+        public void Configuration(IAppBuilder app) {
+            ConfigureAuth(app);
+        }
+    }
+}
